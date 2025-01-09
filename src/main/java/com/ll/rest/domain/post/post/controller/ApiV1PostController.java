5b2103a1d524eb2ea3ef9203f968bbfb624abe6e
son <handsun000@gmail.com>
2025-01-10 00:36:48 +0900

    work

--- src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
@@ -67,7 +67,7 @@ public class ApiV1PostController {
 
     @PutMapping("/{id}")
     @Transactional
-    public RsData modifyItem(
+    public RsData<PostDto> modifyItem(
             @PathVariable long id,
             @RequestBody @Valid PostModifyReqBody reqBody
     ) {
@@ -75,7 +75,7 @@ public class ApiV1PostController {
 
         postService.modify(post, reqBody.title, reqBody.content);
 
-        return new RsData(
+        return new RsData<>(
                 "200-1",
                 "%d번 글이 수정되었습니다.".formatted(id),
                 new PostDto(post)
@@ -94,15 +94,15 @@ public class ApiV1PostController {
     }
 
     @PostMapping()
-    public RsData writeItem(
+    public RsData<Long> writeItem(
             @RequestBody @Valid PostModifyReqBody reqBody
     ) {
         Post post = postService.write(reqBody.title, reqBody.content);
 
-        return new RsData(
+        return new RsData<>(
                 "200-1",
                 "%d번 글이 작성되었습니다.".formatted(post.getId()),
-                new PostDto(post)
+                post.getId()
         );
     }
 }
