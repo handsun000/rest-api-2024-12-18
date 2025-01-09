9b72379406625ea4ca51e962735a482e079c3785
son <handsun000@gmail.com>
2025-01-09 17:26:15 +0900

    work

---
+++ src/main/java/com/ll/rest/domain/post/post/service/PostService.java
@@ -0,0 +1,14 @@
+package com.ll.rest.domain.post.post.service;
+
+import com.ll.rest.domain.post.post.entity.Post;
+import com.ll.rest.domain.post.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+
+}
