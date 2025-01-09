c670e868e5dbc2a4e2a8984d69c55e7f8a3569db
son <handsun000@gmail.com>
2025-01-09 19:14:37 +0900

    work

--- src/main/java/com/ll/rest/global/jpa/entity/BaseTime.java
+++ src/main/java/com/ll/rest/global/jpa/entity/BaseTime.java
@@ -17,8 +17,8 @@ import java.time.LocalDateTime;
 public class BaseTime extends BaseEntity {
     @CreatedDate
     @Setter(AccessLevel.PRIVATE)
-    private LocalDateTime createdAt;
+    private LocalDateTime createDate;
     @LastModifiedDate
     @Setter(AccessLevel.PRIVATE)
-    private LocalDateTime modifiedAt;
+    private LocalDateTime modifyDate;
 }
