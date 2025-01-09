e3b8ba13bec8c80c818adb06c6322b1d6084524f
son <handsun000@gmail.com>
2025-01-09 18:06:04 +0900

    work

--- src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
@@ -2,10 +2,13 @@ package com.ll.rest.domain.post.post.controller;
 
 import com.ll.rest.domain.post.post.entity.Post;
 import com.ll.rest.domain.post.post.service.PostService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -27,13 +30,41 @@ public class ApiV1PostController {
     }
 
     @DeleteMapping("/{id}")
-    public String deleteItem(
+    public Map<String, Object> deleteItem(
             @PathVariable long id
     ) {
         Post post = postService.findById(id).get();
 
         postService.delete(post);
 
-        return "삭제가 완료되었습니다.";
+        Map<String,Object> rsData = new HashMap<>();
+        rsData.put("resultCode", "200-1");
+        rsData.put("msg", "%d번 글을 삭제하였습니다.".formatted(id));
+
+        return rsData;
+    }
+
+    record PostModifyReqBody(
+            String title,
+            String content
+    ) {
+
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public Map<String, Object> modifyItem(
+            @PathVariable long id,
+            @RequestBody PostModifyReqBody reqBody
+    ) {
+        Post post = postService.findById(id).get();
+
+        postService.modify(post, reqBody.title, reqBody.content);
+
+        Map<String, Object> rsData = new HashMap<>();
+        rsData.put("resultCode", "200-1");
+        rsData.put("msg", "%d번 글을 수정되었습니다.".formatted(id));
+
+        return rsData;
     }
 }
