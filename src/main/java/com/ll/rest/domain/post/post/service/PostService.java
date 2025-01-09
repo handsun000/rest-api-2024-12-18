3fe666e39ba5c955a2feab90521c451bd71f04c0
son <handsun000@gmail.com>
2025-01-09 17:51:17 +0900

    work

--- src/main/java/com/ll/rest/domain/post/post/service/PostService.java
+++ src/main/java/com/ll/rest/domain/post/post/service/PostService.java
@@ -34,4 +34,8 @@ public class PostService {
     public Optional<Post> findById(long id) {
         return postRepository.findById(id);
     }
+
+    public void delete(Post post) {
+        postRepository.delete(post);
+    }
 }
