e038af56c7c0097310ce076c20a5d06744e400dd
son <handsun000@gmail.com>
2025-01-10 10:11:11 +0900

    work

--- src/main/java/com/ll/rest/global/globalExceptionHandlers/GlobalExceptionHandler.java
+++ src/main/java/com/ll/rest/global/globalExceptionHandlers/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,5 +26,19 @@ public class GlobalExceptionHandler {
                         "404-1",
                         "해당 데이터가 존재하지 않습니다."
                 ));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<RsData<Void>> handle(MethodArgumentNotValidException ex) {
+
+        String message = ex.getBindingResult().getFieldError().getDefaultMessage();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(
+                        new RsData<>(
+                                "400-1",
+                                message
+                        ));
     }
 }
