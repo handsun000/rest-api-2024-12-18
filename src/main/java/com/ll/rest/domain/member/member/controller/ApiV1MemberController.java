c894d071a6b774de1d2cb9e890e3acd292df8254
son <handsun000@gmail.com>
2025-01-21 12:03:42 +0900

    work

--- src/main/java/com/ll/rest/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/rest/domain/member/member/controller/ApiV1MemberController.java
@@ -1,94 +1,19 @@
 package com.ll.rest.domain.member.member.controller;
 
-import com.ll.rest.domain.member.member.dto.MemberDto;
-import com.ll.rest.domain.member.member.entity.Member;
-import com.ll.rest.domain.member.member.service.MemberService;
-import com.ll.rest.global.exception.ServiceException;
-import com.ll.rest.global.rq.Rq;
 import com.ll.rest.global.rsData.RsData;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.validator.constraints.Length;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
 public class ApiV1MemberController {
-    private final MemberService memberService;
-    private final Rq rq;
-
-    record MemberJoinReqBody(
-            @NotBlank
-            @Length(min = 4)
-            String username,
-            @NotBlank
-            @Length(min = 4)
-            String password,
-            @NotBlank
-            @Length(min = 2)
-            String nickname
-    ) {
-
-    }
 
     @PostMapping("/join")
-    public RsData<MemberDto> join(
-            @RequestBody @Valid MemberJoinReqBody reqBody
-    ) {
-        Member member = memberService.join(reqBody.username, reqBody.password, reqBody.nickname);
-        return new RsData<>(
-                "201-1",
-                "%s님 환영합니다".formatted(member.getNickname()),
-                new MemberDto(member)
-        );
-    }
-
-    record MemberLoginResBody(
-            @NotBlank
-            @Length(min = 4)
-            String username,
-            @NotBlank
-            @Length(min = 4)
-            String password
-    ) {
-    }
-
-    record MemberLoginReqBody(
-            MemberDto item,
-            String apiKey
-    ) {
-    }
-
-    @PostMapping("/login")
-    public RsData<MemberLoginReqBody> login(
-            @RequestBody @Valid MemberLoginResBody reqBody
-    ) {
-        Member member = memberService.findByUsername(reqBody.username)
-                .orElseThrow(() -> new ServiceException("401-1", "해당 회원은 존재하지 않습니다."));
-
-        if (!member.getPassword().equals(reqBody.password)) {
-            throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
-        }
-
-        return new RsData<>(
-                "200-1",
-                "%s님 환영합니다".formatted(member.getNickname()),
-                new MemberLoginReqBody(
-                        new MemberDto(member),
-                        member.getApiKey()
-                )
-        );
-    }
-
-    @GetMapping("/me")
-    public MemberDto me() {
-        Member actor = rq.checkAuthentication();
-
-        return new MemberDto(actor);
+    public RsData<Void> join() {
+        return new RsData<>("201-1", "무명님 환영합니다.");
     }
 }
---
+++ src/test/java/com/ll/rest/domain/member/member/controller/ApiV1MemberControllerTest.java
@@ -0,0 +1,36 @@
+package com.ll.rest.domain.member.member.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Transactional
+public class ApiV1MemberControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("회원가입")
+    void t1() throws Exception {
+        mvc
+                .perform(post("/api/v1/members/join"))
+                .andDo(print())
+                .andExpect(handler().handlerType(ApiV1MemberController.class))
+                .andExpect(handler().methodName("join"))
+                .andExpect(status().isCreated());
+
+    }
+}
