c0c12bbf762421d1a8de08d269e0cea0f62957e5
son <handsun000@gmail.com>
2025-01-22 09:55:33 +0900

    work

--- src/main/java/com/ll/rest/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/rest/domain/member/member/controller/ApiV1MemberController.java
@@ -1,19 +1,30 @@
 package com.ll.rest.domain.member.member.controller;
 
+import com.ll.rest.domain.member.member.entity.Member;
+import com.ll.rest.domain.member.member.service.MemberService;
 import com.ll.rest.global.rsData.RsData;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/members")
 public class ApiV1MemberController {
 
+    private final MemberService memberService;
+
+    record MemberJoinReqBody(
+            String username,
+            String password,
+            String nickname
+    ){}
+
     @PostMapping("/join")
-    public RsData<Void> join() {
-        return new RsData<>("201-1", "무명님 환영합니다.");
+    public RsData<Void> join(@RequestBody MemberJoinReqBody reqBody) {
+        Member member = memberService.join(reqBody.username, reqBody.password, reqBody.nickname);
+
+        return new RsData<>("201-1",
+                "%s님 환영합니다. 회원가입이 완료되었습니다.".formatted(member.getNickname()));
     }
 }
--- src/test/java/com/ll/rest/domain/member/member/controller/ApiV1MemberControllerTest.java
+++ src/test/java/com/ll/rest/domain/member/member/controller/ApiV1MemberControllerTest.java
@@ -1,18 +1,23 @@
 package com.ll.rest.domain.member.member.controller;
 
+import com.ll.rest.domain.member.member.entity.Member;
+import com.ll.rest.domain.member.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -22,15 +27,35 @@ public class ApiV1MemberControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private MemberService memberService;
+
     @Test
     @DisplayName("회원가입")
     void t1() throws Exception {
         mvc
-                .perform(post("/api/v1/members/join"))
+                .perform(
+                        post("/api/v1/members/join")
+                                .content(
+                                        """
+                                        {
+                                            "username" : "usernew",
+                                            "password" : "1234",
+                                            "nickname" : "무명"
+                                        }
+                                        """.stripIndent()
+                                )
+                                .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+                )
                 .andDo(print())
                 .andExpect(handler().handlerType(ApiV1MemberController.class))
                 .andExpect(handler().methodName("join"))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.resultCode").value("201-1"))
+                .andExpect(jsonPath("$.msg").value("무명님 환영합니다. 회원가입이 완료되었습니다."));
+
+        Member member = memberService.findByUsername("usernew").get();
+        assertThat(member.getNickname()).isEqualTo("무명");
 
     }
 }
