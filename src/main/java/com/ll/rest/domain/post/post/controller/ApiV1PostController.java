029279143254c6ec07ac318eef5ee5ef42a605a1
son <handsun000@gmail.com>
2025-01-23 12:24:26 +0900

    work

--- src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
@@ -92,4 +92,21 @@ public class ApiV1PostController {
                 new PostDto(post)
         );
     }
+
+    @DeleteMapping("/{id}")
+    public RsData<Void> delete(
+            @PathVariable long id
+    ) {
+        Member member = rq.checkAuthentication();
+        Post post = postService.findById(id).get();
+
+        post.checkActorCanDelete(member);
+
+        postService.delete(post);
+
+        return new RsData<>(
+                "200-1",
+                "%s번 글이 삭제되었습니다.".formatted(id)
+        );
+    }
 }
--- src/test/java/com/ll/rest/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/ll/rest/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -291,4 +291,24 @@ public class ApiV1PostControllerTest {
                 .andExpect(jsonPath("$.resultCode").value("403-2"))
                 .andExpect(jsonPath("$.msg").value("작성자만 글을 수정할 권한이 있습니다."));
     }
+
+    @Test
+    @DisplayName("글 삭제")
+    void t10() throws Exception {
+        Member member = memberService.findByUsername("user1").get();
+
+        mvc
+                .perform(
+                        delete("/api/v1/posts/1")
+                                .header("Authorization", "Bearer " + member.getApiKey())
+                )
+                .andDo(print())
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("delete"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultCode").value("200-1"))
+                .andExpect(jsonPath("$.msg").value("1번 글이 삭제되었습니다."));
+
+        assertThat(postService.findById(1)).isEmpty();
+    }
 }
