e38bc4272b72215faff2b01f185c90ba452435ed
son <handsun000@gmail.com>
2025-01-23 12:06:26 +0900

    work

--- src/test/java/com/ll/rest/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/ll/rest/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -205,4 +205,37 @@ public class ApiV1PostControllerTest {
                 .andExpect(jsonPath("$.data.title").value("제목 new"))
                 .andExpect(jsonPath("$.data.content").value("내용 new"));
     }
+
+    @Test
+    @DisplayName("글 수정, with no input ")
+    void t7() throws Exception {
+
+        Member member = memberService.findByUsername("user1").get();
+
+        ResultActions resultActions = mvc
+                .perform(
+                        put("/api/v1/posts/1")
+                                .header("Authorization", "Bearer " + member.getApiKey())
+                                .content("""
+                                        {
+                                            "title" : "",
+                                            "content" : ""
+                                        }
+                                        """)
+                                .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("modify"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.resultCode").value("400-1"))
+                .andExpect(jsonPath("$.msg").value("""
+                        content-Length-length must be between 2 and 10000000
+                        content-NotBlank-must not be blank
+                        title-Length-length must be between 2 and 100
+                        title-NotBlank-must not be blank
+                        """.stripIndent().trim()));
+    }
 }
