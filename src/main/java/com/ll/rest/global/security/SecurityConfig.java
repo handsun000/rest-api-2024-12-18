05bc2c35f1b4c567c49b55457a7bc5bf1b0316d0
son <handsun000@gmail.com>
2025-02-05 11:06:55 +0900

    work

---
+++ src/main/java/com/ll/rest/global/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.ll.rest.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/h2-console/**")
+                                .permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/*/posts/{id:\\d+}", "/api/*/posts", "/api/*/posts/{postId:\\d+}/comments/{id:\\d+}")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .headers(
+                        headers ->
+                                headers.frameOptions(
+                                        frameOptions ->
+                                                frameOptions.sameOrigin()
+                                )
+                )
+                .csrf(
+                        csrf ->
+                                csrf.disable()
+                );
+
+        return http.build();
+    }
+}
