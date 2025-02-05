bc7a57f79368ec640e975e4188c1f98ea60ce242
son <handsun000@gmail.com>
2025-02-05 14:45:24 +0900

    work

--- src/main/java/com/ll/rest/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/ll/rest/global/security/CustomAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.ll.rest.global.security;
 
+import com.ll.rest.domain.member.member.entity.Member;
+import com.ll.rest.domain.member.member.service.MemberService;
 import com.ll.rest.global.rq.Rq;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -11,16 +13,41 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class CustomAuthenticationFilter extends OncePerRequestFilter {
 
     private final Rq rq;
+    private final MemberService memberService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        rq.setLogin("user1");
+        if (!request.getRequestURI().startsWith("/api/")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String apiKey = authorization.substring("Bearer ".length());
+
+        Optional<Member> opMember = memberService.findByApiKey(apiKey);
+
+        if (opMember.isEmpty()) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        Member member = opMember.get();
+
+        rq.setLogin(member.getUsername());
 
         filterChain.doFilter(request, response);
     }
