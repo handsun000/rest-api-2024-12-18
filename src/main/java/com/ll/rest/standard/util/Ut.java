eec292c92f668ca79631553746828f9f735b822e
son <handsun000@gmail.com>
2025-02-21 14:18:34 +0900

    work

--- src/main/java/com/ll/rest/standard/util/Ut.java
+++ src/main/java/com/ll/rest/standard/util/Ut.java
@@ -1,7 +1,17 @@
 package com.ll.rest.standard.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ClaimsBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.SneakyThrows;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 
 public class Ut {
     public static class str {
@@ -17,6 +27,23 @@ public class Ut {
         @SneakyThrows
         public static String toString(Object obj) {
             return om.writeValueAsString(obj);
+        }
+    }
+    public static class jwt {
+
+        public static String toString(String secret, int expireSeconds, Map<String,Object> body) {
+
+            Date issuedAt = new Date();
+            Date expiration = new Date(issuedAt.getTime() + 1000L + expireSeconds);
+
+            Key secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+
+            return Jwts.builder()
+                    .claims(body)
+                    .issuedAt(issuedAt)
+                    .expiration(expiration)
+                    .signWith(secretKey)
+                    .compact();
         }
     }
 }
--- src/test/java/com/ll/rest/domain/member/member/service/AuthTokenServiceTest.java
+++ src/test/java/com/ll/rest/domain/member/member/service/AuthTokenServiceTest.java
@@ -1,6 +1,8 @@
 package com.ll.rest.domain.member.member.service;
 
+import com.ll.rest.domain.member.member.entity.Member;
 import com.ll.rest.domain.member.member.service.AuthTokenService;
+import com.ll.rest.standard.util.Ut;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -14,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,6 +27,12 @@ public class AuthTokenServiceTest {
     @Autowired
     private AuthTokenService authTokenService;
 
+    @Autowired
+    private MemberService memberService;
+
+    private int expireSeconds = 60 * 60 * 24 * 365;
+    private String secret = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890";
+
     @Test
     @DisplayName("authTokenService 서비스가 존재한다.")
     void t1() {
@@ -33,9 +42,6 @@ public class AuthTokenServiceTest {
     @Test
     @DisplayName("jjwt 로 JWT 생성, {name=\"Paul\", age=23}")
     void t2() {
-        int expireSeconds = 60 * 60 * 24 * 365;
-        Key secretKey = Keys.hmacShaKeyFor("abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes());
-
         Claims claims = Jwts.claims()
                 .add("name", "Paul")
                 .add("age", 23)
@@ -43,6 +49,8 @@ public class AuthTokenServiceTest {
 
         Date issuedAt = new Date();
         Date expiration = new Date(issuedAt.getTime() + 1000L * expireSeconds);
+
+        Key secretKey = Keys.hmacShaKeyFor(secret.getBytes());
 
         String jwt = Jwts.builder()
                 .setClaims(claims)
@@ -52,5 +60,23 @@ public class AuthTokenServiceTest {
                 .compact();
 
         assertThat(jwt).isNotBlank();
+    }
+
+    @Test
+    @DisplayName("")
+    void t3() {
+        String jwt = Ut.jwt.toString(secret, expireSeconds, Map.of("name", "Paul", "age", "23"));
+
+        assertThat(jwt).isNotBlank();
+    }
+
+    @Test
+    @DisplayName("authTokenService.genAccessToken(member)")
+    void t4() {
+        Member member = memberService.findByUsername("user1").get();
+
+        String accessToken = authTokenService.genAccessToken(member);
+
+        assertThat(accessToken).isNotBlank();
     }
 }
