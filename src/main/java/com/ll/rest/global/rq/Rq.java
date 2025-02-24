199b34fb053e782c34b4cf6e8b2b9114d4dd9137
son <handsun000@gmail.com>
2025-02-24 12:26:49 +0900

    work

--- src/main/java/com/ll/rest/global/rq/Rq.java
+++ src/main/java/com/ll/rest/global/rq/Rq.java
@@ -1,22 +1,21 @@
 package com.ll.rest.global.rq;
 
 import com.ll.rest.domain.member.member.entity.Member;
-import com.ll.rest.domain.member.member.service.MemberService;
-import com.ll.rest.global.exception.ServiceException;
 import com.ll.rest.global.security.SecurityUser;
-import com.ll.rest.standard.util.Ut;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
-import java.util.List;
+import javax.swing.text.html.Option;
+import java.util.Arrays;
 import java.util.Optional;
 
 @RequestScope
@@ -24,8 +23,8 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class Rq {
 
+    private final HttpServletResponse response;
     private final HttpServletRequest request;
-    private final MemberService memberService;
 
     public void setLogin(Member member) {
         UserDetails user = new SecurityUser(
@@ -55,5 +54,34 @@ public class Rq {
                 .map(principal -> (SecurityUser) principal)
                 .map(securityUser -> new Member(securityUser.getId(), securityUser.getUsername()))
                 .orElse(null);
+    }
+
+    public void setCookie(String key, String value) {
+        ResponseCookie cookie = ResponseCookie.from(key, value)
+                .path("/")
+                .domain("localhost")
+                .sameSite("Strict")
+                .secure(true)
+                .httpOnly(true)
+                .build();
+
+        response.addHeader("Set-Cookie", cookie.toString());
+    }
+
+    public String getCookieValue(String key) {
+        return Optional
+                .ofNullable(
+                        request.getCookies()
+                )
+                .stream()
+                .flatMap(Arrays::stream)
+                .filter(cookie -> cookie.getName().equals(key))
+                .map(Cookie::getValue)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public void setHeader(String key, String value) {
+        response.setHeader(key, value);
     }
 }
